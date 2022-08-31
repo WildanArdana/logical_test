572221b43da38eaaa116e0ba7a37929d737e7dac
WildanArdana <kamissalimd@gmail.com>
2022-08-31 10:38:57 +0700

    first commit

---
+++ 1_array_loop.php
@@ -0,0 +1,10 @@
+<?php
+
+$aplikasi[1] = 'gtAkademik';
+$aplikasi[2] = 'gtFinansi';
+$aplikasi[3] = 'gtPerizinan';
+$aplikasi[4] = 'eCampuz';
+$aplikasi[5] = 'eOviz';
+
+echo "aplikasi ". $aplikasi[1],", $aplikasi[2],", $aplikasi[3],", $aplikasi[4],", $aplikasi[5] . " adalah data";
+?>
---
+++ 3_operator_logic.php
@@ -0,0 +1,7 @@
+<?php
+$a = 8;
+    $b = 4;
+
+    $total = $a /= $b;
+
+    echo $total;
